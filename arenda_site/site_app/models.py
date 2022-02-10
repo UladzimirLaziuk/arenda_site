a4d857bd498d13785aefc87d7f17935bea8425c7
vladimir <1614741@mail.ru>
2022-02-10 23:22:23 +0300

    edit models

--- arenda_site/site_app/models.py
+++ arenda_site/site_app/models.py
@@ -7,13 +7,13 @@ class Renter(models.Model):
     name_organization = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)  # ArrayField
     location = models.CharField(max_length=255)  # modul
-    # types_of_services = ArrayField(models.CharField(max_length=255, blank=True), default=list) TODO ILIA HELP
+    # types_of_services = ArrayField(models.CharField(max_length=255, blank=True), default=list)
     types_of_services = models.ManyToManyField('TypeService', blank=True)  # OR Arrayfield
     delivery = models.PositiveIntegerField(null=True, default=0)
     price_per_hour = models.PositiveIntegerField()
-    types_of_buckets = models.ForeignKey('Buckets', on_delete=models.SET_NULL, blank=True, null=True)
-    other_types_of_communication = models.ManyToManyField('Communication', blank=True)  # ??Choises
-    type_of_tractor = models.ForeignKey('Vehicle', on_delete=models.CASCADE)
+    # types_of_buckets = models.ForeignKey('Buckets', on_delete=models.SET_NULL, blank=True, null=True)
+    # other_types_of_communication = models.ManyToManyField('Communication', blank=True)  # ??Choises
+    # type_of_tractor = models.ForeignKey('Vehicle', on_delete=models.CASCADE)
     email = models.EmailField(blank=True)
 
     def __str__(self):
@@ -28,24 +28,29 @@ class TypeService(models.Model):
 
 
 class Buckets(models.Model):
-    renter_obj = models.ForeignKey('Renter', on_delete=models.CASCADE, blank=True, null=True, related_name='renter_object')
+    renter_obj = models.ForeignKey('Renter', on_delete=models.CASCADE, blank=True, null=True, related_name='buckets')
+    size_bucket = models.CharField(max_length=20)
+
 
 
 class Communication(models.Model):
-    pass
+    other_types_of_communication = models.ForeignKey('Renter', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=255)
 
 
 class AdditionalEquipment(models.Model):
-    pass
+    equipment = models.ForeignKey('Renter', on_delete=models.CASCADE)
+    type_equipment = models.CharField(max_length=1)
 
 
 class Vehicle(models.Model):
     name_brand = models.CharField(max_length=255)
     additional_equipment = models.ForeignKey('AdditionalEquipment', on_delete=models.SET_NULL, blank=True,
                                              null=True)  # Arrayfield
-    weight = models.PositiveIntegerField()
-    max_digging_depth = models.PositiveIntegerField()  # string or float
-    vehicle_height = models.PositiveIntegerField()
+    renter = models.ForeignKey('Renter', on_delete=models.CASCADE)
+    weight = models.CharField(max_length=50)
+    max_digging_depth = models.CharField(max_length=50)  # string or float
+    vehicle_height = models.CharField(max_length=50)
 
     def __str__(self):
         return f'{self.name_brand}'
